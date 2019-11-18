f22b52ddf18a46ddde4ae0ab610ae9161a0468c9
Brian Icaro D. Marques <bi.dm@hotmail.com>
2019-11-18 16:24:38 -0300

    First commit

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
-var ulElement = document.querySelector(".repositorios");
-var formElement = document.querySelector(".form");
+var ulElement = document.querySelector('.repositorios');
+var formElement = document.querySelector('.form');
 
-formElement.addEventListener("submit", function pesquisar(event) {
+formElement.addEventListener('submit', function pesquisar(event) {
   event.preventDefault();
 
-  var inputElement = document.querySelector("form__input");
-  var user = inputElement.text;
-
+  var inputElement = document.querySelector('input');
+  var user = inputElement.value;
   exibirMensagemCarregando();
 
   axios
-    .get("https://api.github.com/users/" + user + "/repos")
+    .get('https://api.github.com/users/' + user + '/repos')
     .then(function(response) {
+      removeMensagemCarregando();
       listarRepositorios(response.data);
     })
     .catch(function(error) {
@@ -22,18 +22,22 @@ formElement.addEventListener("submit", function pesquisar(event) {
 });
 
 function exibirMensagemCarregando() {
-  ulElement.innerHTML = "";
-  adicionarItemLista("Carregando...");
+  ulElement.innerHTML = '';
+  adicionarItemLista('Carregando...');
+}
+function removeMensagemCarregando() {
+  var element = document.querySelector('ul');
+  element.removeChild(element.childNodes[0]);
 }
 
 function exibirMensagemErro() {
-  ulElement.innerHTML = "";
-  adicionarItemLista("Falha ao carregar a lista de repositórios!");
+  ulElement.innerHTML = '';
+  adicionarItemLista('Falha ao carregar a lista de repositórios!');
 }
 
 function listarRepositorios(repositories) {
   if (repositories.length === 0) {
-    adicionarItemLista("Nenhum repositório encontrado");
+    adicionarItemLista('Nenhum repositório encontrado');
   }
 
   for (var repo of repositories) {
@@ -43,7 +47,7 @@ function listarRepositorios(repositories) {
 
 function adicionarItemLista(texto) {
   var textElement = document.createTextNode(texto);
-  var loadingElement = document.createElement("li");
+  var loadingElement = document.createElement('li');
   loadingElement.appendChild(textElement);
   ulElement.appendChild(loadingElement);
 }
